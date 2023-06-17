5fdeeb82e72d61328bdbbdd6a4e317da644f65e6
Bruno Tenório <tenoriobn@gmail.com>
2023-06-17 13:53:43 -0300

    Criando aoDeletar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,13 @@
-import './Colaborador.css'
+import './colaborador.css'
 
-const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
-    return (<div className='colaborador'>
-        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-            <img src={imagem} alt={nome}/>
+const Colaborador = ({ colaborador, corDeFundo }) => {
+    return (<div className="colaborador">
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
-        <div className='rodape'>
-            <h4>{nome}</h4>
-            <h5>{cargo}</h5>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
         </div>
     </div>)
 }
